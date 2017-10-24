b5099f161a7ffdf4d7bafbfda275ada9bdfdad58
Nolan Baker <nolan.lee.baker@gmail.com>
2017-10-24 12:09:59 -0500

    lists texture channels, gets paint uv

--- Editor/RobBossEditor.cs
+++ Editor/RobBossEditor.cs
@@ -6,14 +6,17 @@ using UnityEditor;
 
 public class RobBossEditor : EditorWindow {
 
+	static int canvasID = 0;
 	static bool directional = false;
 	static Color color = Color.white;
 	static float radius = 0.5f;
 	static float blend = 0.1f;
 
+	static string[] canvasNames = new string[0];
 	static MeshCollider raycastTarget;
 	static Mesh colliderMesh;
 	static Renderer paintTarget;
+	static Vector2? uv;
 
 	[MenuItem ("Window/Rob Boss Painter")]
 	static void Open () {
@@ -54,18 +57,30 @@ public class RobBossEditor : EditorWindow {
 		EditorPrefs.SetFloat("RobBoss.Blend", blend);
     }
 
+	void OnGUI () {
+		canvasID = EditorGUILayout.Popup("Canvas", canvasID, canvasNames);
+		directional = EditorGUILayout.Toggle("Directional", directional);
+		GUI.enabled = !directional;
+		color = EditorGUILayout.ColorField("Color", color);
+		GUI.enabled = true;
+		radius = EditorGUILayout.FloatField("Radius", radius);
+		blend = EditorGUILayout.FloatField("Blend", blend);
+	}
+
 	public static void OnSceneGUI(SceneView sceneview) {			
 		UpdateTarget();
-		DrawBrush();
+		RaycastTarget();
 	}
 
 	static void UpdateTarget() {
 		if (Selection.activeGameObject == null) return;
-		
+
 		Renderer r = Selection.activeGameObject.GetComponentInChildren<Renderer>();
 		if (r != null && r != paintTarget) {
 			paintTarget = r;
-	
+			UpdateCanvasNames();
+			colliderMesh.Clear();
+
 			if (paintTarget is MeshRenderer) {
 				MeshFilter f = paintTarget.GetComponent<MeshFilter>();
 				if (f != null && f.sharedMesh != null) {
@@ -84,26 +99,37 @@ public class RobBossEditor : EditorWindow {
 			}
 
 			raycastTarget.sharedMesh = colliderMesh;
+			raycastTarget.transform.position = paintTarget.transform.position;
+			raycastTarget.transform.rotation = paintTarget.transform.rotation;
+			raycastTarget.transform.localScale = paintTarget.transform.localScale;
 		}
 	}
 
-	static void DrawBrush() {	
+	static void RaycastTarget() {	
 		if (raycastTarget == null) return;
+
 		Handles.color = color;	 
 		Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
 		RaycastHit hit;
 		if (raycastTarget.Raycast(ray, out hit, Mathf.Infinity)) {
 			Handles.DrawLine(hit.point, hit.point + hit.normal * 2);
-			Handles.DrawWireDisc(hit.point, hit.normal, radius * paintTarget.bounds.extents.y);		
+			Handles.DrawWireDisc(hit.point, hit.normal, radius * paintTarget.bounds.extents.y);
+			uv = hit.textureCoord;
 		} 
+		else {
+			uv = null;
+		}
 	}
 
-	void OnGUI () {
-		directional = EditorGUILayout.Toggle("Directional", directional);
-		GUI.enabled = !directional;
-		color = EditorGUILayout.ColorField("Color", color);
-		GUI.enabled = true;
-		radius = EditorGUILayout.FloatField("Radius", radius);
-		blend = EditorGUILayout.FloatField("Blend", blend);
+	static void UpdateCanvasNames() {
+		List<string> names = new List<string>();
+		Shader shader = paintTarget.sharedMaterial.shader;
+    	for (int i = 0; i < ShaderUtil.GetPropertyCount(shader); i++) {
+			if (ShaderUtil.IsShaderPropertyHidden(shader, i)) continue;
+    		if (ShaderUtil.GetPropertyType(shader, i) == ShaderUtil.ShaderPropertyType.TexEnv) {
+    			names.Add(ShaderUtil.GetPropertyName(shader, i));
+			}
+    	}
+		canvasNames = names.ToArray();
 	}
 }
