c26294cfc40d655df09cefd00bc8fba4a1ac5f46
Nolan Baker <nolan.lee.baker@gmail.com>
2017-10-24 11:22:05 -0500

    adds brush shader, stubbs out editor window

---
+++ Editor/RobBossEditor.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class RobBossEditor : EditorWindow {
+
+	static bool directional = false;
+	static Color color = Color.white;
+	static float radius = 0.5f;
+	static float blend = 0.1f;
+
+	static MeshCollider raycastTarget;
+	static Mesh colliderMesh;
+	static Renderer paintTarget;
+
+	[MenuItem ("Window/Rob Boss Painter")]
+	static void Open () {
+		RobBossEditor window = EditorWindow.GetWindow(typeof(RobBossEditor)) as RobBossEditor;
+		window.minSize = new Vector2(250, 360);
+		window.Show();
+	}
+
+	static SceneView.OnSceneFunc onSceneFunc;
+    void OnEnable () {
+   		if (onSceneFunc == null) onSceneFunc = new SceneView.OnSceneFunc(OnSceneGUI);
+		SceneView.onSceneGUIDelegate += onSceneFunc;
+
+		GameObject g = new GameObject("RobBossTarget");
+		g.hideFlags = HideFlags.HideAndDontSave;
+		raycastTarget = g.AddComponent<MeshCollider>();
+		colliderMesh = new Mesh();
+		colliderMesh.name = "RobBossColliderMesh";
+		colliderMesh.hideFlags = HideFlags.HideAndDontSave;
+		raycastTarget.sharedMesh = colliderMesh;
+
+		directional = EditorPrefs.GetInt("RobBoss.Directional", 0) == 1;
+		string colorString = EditorPrefs.GetString("RobBoss.Color", "#FFFFFFFF");
+		ColorUtility.TryParseHtmlString(colorString, out color);
+		radius = EditorPrefs.GetFloat("RobBoss.Radius", 0.5f);
+		blend = EditorPrefs.GetFloat("RobBoss.Blend", 0.1f);
+    }
+
+    void OnDisable () {
+    	SceneView.onSceneGUIDelegate -= onSceneFunc;
+
+		DestroyImmediate(colliderMesh);
+		DestroyImmediate(raycastTarget.gameObject);
+
+		EditorPrefs.SetInt("RobBoss.Directional", directional ? 1 : 0);
+		EditorPrefs.SetString("RobBoss.Color", ColorUtility.ToHtmlStringRGBA(color));
+		EditorPrefs.SetFloat("RobBoss.Radius", radius);
+		EditorPrefs.SetFloat("RobBoss.Blend", blend);
+    }
+
+	public static void OnSceneGUI(SceneView sceneview) {			
+		UpdateTarget();
+		DrawBrush();
+	}
+
+	static void UpdateTarget() {
+		if (Selection.activeGameObject == null) return;
+		
+		Renderer r = Selection.activeGameObject.GetComponentInChildren<Renderer>();
+		if (r != null && r != paintTarget) {
+			paintTarget = r;
+	
+			if (paintTarget is MeshRenderer) {
+				MeshFilter f = paintTarget.GetComponent<MeshFilter>();
+				if (f != null && f.sharedMesh != null) {
+					colliderMesh.vertices = f.sharedMesh.vertices;
+					colliderMesh.uv = f.sharedMesh.uv;
+					colliderMesh.triangles = f.sharedMesh.triangles;
+				}
+			}
+			else if (paintTarget is SpriteRenderer) {
+				Sprite s = (paintTarget as SpriteRenderer).sprite;
+				if (s != null) {
+					colliderMesh.vertices = Array.ConvertAll(s.vertices, (v) => (Vector3)v);
+					colliderMesh.uv = s.uv;
+					colliderMesh.triangles = Array.ConvertAll(s.triangles, (t) => (int)t);
+				}
+			}
+
+			raycastTarget.sharedMesh = colliderMesh;
+		}
+	}
+
+	static void DrawBrush() {	
+		if (raycastTarget == null) return;
+		Handles.color = color;	 
+		Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+		RaycastHit hit;
+		if (raycastTarget.Raycast(ray, out hit, Mathf.Infinity)) {
+			Handles.DrawLine(hit.point, hit.point + hit.normal * 2);
+			Handles.DrawWireDisc(hit.point, hit.normal, radius * paintTarget.bounds.extents.y);		
+		} 
+	}
+
+	void OnGUI () {
+		directional = EditorGUILayout.Toggle("Directional", directional);
+		GUI.enabled = !directional;
+		color = EditorGUILayout.ColorField("Color", color);
+		GUI.enabled = true;
+		radius = EditorGUILayout.FloatField("Radius", radius);
+		blend = EditorGUILayout.FloatField("Blend", blend);
+	}
+}
---
+++ Shaders/RobBossBrushShader.shader
@@ -0,0 +1,68 @@
+Shader "Rob Boss/Brush" {
+	Properties {
+		_MainTex ("Canvas", 2D) = "white" {}
+		_Brush ("Brush", 2D) = "white" {}
+		_Color ("Color", Color) = (1,1,1,1)
+		_Transform ("UV Coord (X,Y) Angle (Z) Size (W)", Vector) = (0, 0, 0, 1)
+	}
+	SubShader {
+		Tags { 
+			"Queue"="Transparent" 
+			"IgnoreProjector"="True" 
+			"RenderType"="Transparent" 
+			"PreviewType"="Plane"
+			"CanUseSpriteAtlas"="True"
+		}
+
+		Cull Off
+		Lighting Off
+		ZWrite Off
+		Fog { Mode Off }
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass {
+			CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment frag
+				#include "UnityCG.cginc"
+
+				struct appdata_t {
+					float4 pos : POSITION;
+					float2 uv : TEXCOORD0;
+				};
+
+				struct v2f {
+					float4 pos : SV_POSITION;
+					float2 uv_MainTex : TEXCOORD0;
+				};
+						
+				sampler2D _MainTex;
+				float4 _MainTex_ST;
+
+				v2f vert(appdata_t v) {
+					v2f o;
+					o.pos = UnityObjectToClipPos(v.pos);
+					o.uv_MainTex = TRANSFORM_TEX(v.uv, _MainTex);
+					return o;
+				}
+			
+				sampler2D _Brush;
+				float4 _Color;
+				float4 _Transform;
+			
+				float4 frag(v2f IN) : COLOR {
+					float2 uv = (IN.uv_MainTex - _Transform.xy);
+					float a = radians(_Transform.z);
+					uv = float2(uv.x * cos(a) - uv.y * sin(a), uv.y * cos(a) + uv.x * sin(a));
+					uv = uv / _Transform.w + 0.5;
+
+					half4 canvas = tex2D(_MainTex, IN.uv_MainTex);
+					half4 brush = tex2D(_Brush, uv) * _Color;
+					canvas = lerp(canvas, brush, brush.a);
+
+					return canvas;
+				}
+			ENDCG
+		}
+	}
+}
